87b7d58577f01217bc1eeb472c1c6d9ec2704720
beyza.butun <beyza.butun@ozguryazilim.com.tr>
2019-11-06 17:55:31 +0300

    Declare user classes.

---
+++ user.py
@@ -0,0 +1,30 @@
+class user():
+
+    def __init__(self):
+        pass
+
+    def constructor(self, email, name, surname, password):
+        pass
+
+    @staticmethod
+    def verify(self, email, verification_number):
+        pass
+
+    def changepassword(self, newpassword, oldpassword = None):
+        pass
+
+    def lookup(self, emaillist):
+        pass
+
+    def friend(self, email):
+        pass
+
+    def setfriend(self, user, state):
+        pass
+
+    def listitems(self, user):
+        pass
+
+    def watch(self, user, watchmethod):
+        pass
+
