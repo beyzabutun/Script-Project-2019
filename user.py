c8d3a49fcb604c3f34ebe221b0e15cffa4995151
beyza.butun <beyza.butun@ozguryazilim.com.tr>
2019-11-07 21:43:54 +0300

    Update user class and add DBConnection class.

--- user.py
+++ user.py
@@ -1,21 +1,38 @@
 
 class User():
 
+    db_conn = None
+
     def __init__(self, email, name, surname, password):
+        assert self.db_conn
         self.email = email
         self.name = name
         self.surname = surname
         self.password = password
+
+        self.db_conn.insert("Users", (email, name, surname, password) )
 
     @staticmethod
     def verify(self, email, verification_number):
         pass
 
     def change_password(self, new_password, old_password = None):
-        pass
+        if old_password != None:
+
+            self.password = new_password
+            self.db_conn.update("Users", "password", "email", (new_password, self.email))
+
+        #if none generate a temporal password
 
     def look_up(self, email_list):
-        pass
+
+        cur = self.db_conn.cursor()
+        cur.execute("select email from Users u where u.email in ?", (email_list,))
+        fetched = curr.fetchall()
+        if not fetched:
+            return "There are no email from the email_list in the database"
+        self.db_conn.commit()
+        return fetched
 
     def friend(self, email):
         pass
