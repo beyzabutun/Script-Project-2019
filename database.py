fff6dd32b0a7eaae6042f34dc1c2443f186d1945
Beste Burhan <beste.burhan@technarts.com>
2019-11-09 18:14:14 +0300

    merge

--- database.py
+++ database.py
@@ -6,11 +6,14 @@ class DBConnection:
     def __init__(self, path='./data.db'):
         self.connection = sqlite3.connect(path)
         self.curs = self.connection.cursor()
+        self.curs.execute("DROP TABLE IF EXISTS Users;")
+        self.curs.execute("DROP TABLE IF EXISTS Items;")
+        self.curs.execute("DROP TABLE IF EXISTS Borrows;")
         self.curs.execute("CREATE TABLE IF NOT EXISTS Users("
                           "id integer PRIMARY KEY AUTOINCREMENT," +
                           "name char(50)," +
                           "surname char(50)," +
-                          "email char(256) ," +
+                          "email char(256) UNIQUE," +
                           "password char(256)" +
                           ");")
         self.curs.execute("CREATE TABLE IF NOT EXISTS Items(" +
@@ -23,12 +26,14 @@ class DBConnection:
                           "genre char(150)," +
                           "year integer," +
                           "location char(256)," +
-                          "rate integer" +
+                          "rate integer,"
+                          "FOREIGN KEY (owner) REFERENCES Users(id)" +
                           ");")
         self.curs.execute("CREATE TABLE IF NOT EXISTS Friends(" +
                           "user_id1 integer," +
                           "user_id2  integer," +
                           "state integer ," +
+                          "is_verified integer," +
                           "PRIMARY KEY(user_id1, user_id2)," +
                           "FOREIGN KEY (user_id1) REFERENCES Users(id)," +
                           "FOREIGN KEY (user_id2) REFERENCES Users(id)" +
@@ -37,7 +42,7 @@ class DBConnection:
                           "id integer PRIMARY KEY AUTOINCREMENT," +
                           "user_id  integer," +
                           "item_id integer ," +
-                          "FOREIGN KEY (u_email) REFERENCES Users(email)," +
+                          "FOREIGN KEY (user_id) REFERENCES Users(id)," +
                           "FOREIGN KEY (item_id) REFERENCES Items(id)" +
                           ");")
         self.curs.execute("CREATE TABLE IF NOT EXISTS StateItem(" +
@@ -72,16 +77,20 @@ class DBConnection:
 
     def insert(self, table_name, *data):
         cur = self.connection.cursor()
-        cur.execute(f'INSERT INTO {table_name} VALUES {"?,"*len(data)}', data)
+        print(data)
+        f_string = f'INSERT INTO {table_name} VALUES ( {"?,"*(len(data)-1) + "?"} )'
+        cur.execute(f_string, data)
         self.connection.commit()
-
 
     def update(self, table_name, updated_mem, filtered_mem, *data ):
         cur = self.connection.cursor()
+        f_string = f'UPDATE {table_name} SET {updated_mem} = ? WHERE {filtered_mem} = ?'
         cur.execute(
-            f'UPDATE {table_name} SET {updated_mem} = ?, WHERE {filtered_mem} = ?', data)
+            f'UPDATE {table_name} SET {updated_mem} = ? WHERE {filtered_mem} = ?;', data)
         self.connection.commit()
 
 
-
-DBConnection()
+#
+# db = DBConnection()
+# db.insert("Users", None, "beste", "burhan", "email", "password")
+# db.update("Users", "name", "name", "beste", "beste")
--- item.py
+++ item.py
@@ -1,14 +1,30 @@
 # from isbntools.app import *
 from isbnlib import meta, is_isbn10, is_isbn13, _exceptions
+import sqlite3
+import user as _u
+from datetime import datetime, timedelta
+import database as db
 
 
 class Item:
+    REQUEST_TYPES = (
+        (0, "WATCH"),
+        (1, "BORROW")
+    )
+    STATE_TYPE = (
+        (0, "CLOSED"),
+        (1, "FRIENDS"),
+        (2, "CLOSEFRIENDS"),
+        (3, "EVERYONE"),
+    )
 
-    db_conn = None
+    conn = db.DBConnection()
 
     def __init__(self, owner, item_type=None, title=None, uniqid=None, artist=None, genre=None, year=None):
-        self.db_conn = self.db_conn.cursor()
-
+        assert self.conn
+        self.db_conn = self.conn.curs
+        user_id = self.db_conn.execute("select id from Users where email like \'{m}\'".format(m=owner.email))
+        user_id = user_id.fetchone()[0]
         self.owner = owner
         if uniqid is None:
             self.item_type = item_type
@@ -19,8 +35,8 @@ class Item:
             self.year = year
             self.rate = 0
             self.location = None
-            # self.db_conn.execute('''insert into Items values (owner, type, title,
-            # uniqueid, artist, genre, year, location, rate)''', )
+            self.conn.insert("Items", None, user_id, item_type, title, uniqid, artist, genre, year, None, 0)
+            self.id = self.db_conn.execute("select last_insert_rowid()").fetchone()[0]
 
         else:
             metadata = None
@@ -36,28 +52,35 @@ class Item:
                 self.genre = genre
                 self.rate = 0
                 self.location = None
+                self.db_conn.insert("Items", None, user_id, item_type, self.title, uniqid, self.artist, self.genre,
+                                    self.year, None, 0)
 
     def borrowed_req(self, user):
-
-        pass
+        self.conn.insert("Requests", None, user.id, self.id, datetime.now(), self.REQUEST_TYPES[1][0])
 
     def borrowed_by(self, user, return_date=2):
-        pass
+        taking_date = datetime.now()
+        return_date = taking_date + timedelta(weeks=return_date)
+        self.conn.insert("Borrows", None, user.id, self.id, datetime.now(), return_date, 0)
 
     def returned(self, location=None):
-        pass
+        self.db_conn.execute("update Borrows set is_returned=1 where is_returned=0 and item_id={id};".format(id=self.id))
+        self.location = location
+        self.db_conn.execute("update Items set location=\'{loc}\' where id={id};".format(loc=location, id=self.id))
+        self.conn.connection.commit()
 
     def comment(self, user, comment_text):
-        pass
+        self.conn.insert("Comments", None, user.id, self.id, comment_text, datetime.now())
 
     def list_comments(self):
-        pass
+
 
     def rate(self, user, rating):
         self.rate += 1
         pass
 
     def get_rating(self):
+
         pass
 
     def locate(self, location):
@@ -84,3 +107,13 @@ class Item:
     def delete(self):
         pass
 
+
+user = _u.User("beste", "burhan", "beste.com", "password")
+user2 = _u.User("beste", "burhan", "bestee.com", "password")
+user3 = _u.User("beste", "burhan", "besteee.com", "password")
+it = Item(user, "type", "title", None, "artist", "genre", 1996)
+it2 = Item(user, "type", "title", None, "artist", "genre", 1996)
+it3 = Item(user, "type", "title", None, "artist", "genre", 1996)
+it4 = Item(user, "type", "title", None, "artist", "genre", 1996)
+it3.borrowed_by(user)
+it3.returned("location")
