3e7a4d24c3e91a485b65589c2f17717e747d8740
Beste Burhan <beste.burhan@technarts.com>
2019-11-13 21:32:56 +0300

    update

--- item.py
+++ item.py
@@ -3,41 +3,47 @@ from isbnlib import meta, is_isbn10, is_isbn13, _exceptions
 import sqlite3
 import user as _u
 from datetime import datetime, timedelta
-import database as db
+from database import db
 
 
 class Item:
-    REQUEST_TYPES = (
-        (0, "WATCH"),
-        (1, "BORROW")
-    )
-    STATE_TYPE = (
-        (0, "CLOSED"),
-        (1, "FRIENDS"),
-        (2, "CLOSEFRIENDS"),
-        (3, "EVERYONE"),
-    )
+    REQUEST_TYPES = {
+        'WATCH': 0,
+        'BORROW': 1
+    }
 
-    conn = db.DBConnection()
+    STATE = {
+        'NOFRIEND': 0,
+        'CLOSEFRIEND': 1,
+        'FRIEND': 2
+    }
+    STATE_TYPE = {
+        'CLOSED': 0,
+        'CLOSEFRIENDS': 1,
+        'FRIENDS': 2,
+        'EVERYONE': 3,
+    }
+
+    # conn = db.DBConnection()
 
     def __init__(self, owner, item_type=None, title=None, uniqid=None, artist=None, genre=None, year=None):
-        assert self.conn
-        self.db_conn = self.conn.curs
-        user_id = self.db_conn.execute("select id from Users where email like \'{m}\'".format(m=owner.email))
+        # assert self.conn
+        # self.db_conn = self.conn.curs
+        db_cur = db.get_cursor()
+        user_id = db_cur.execute("select id from Users where email like \'{m}\'".format(m=owner.email))
         user_id = user_id.fetchone()[0]
         self.owner = owner
+        self.item_type = item_type
+        self.genre = genre
+        self.rate = 0
+        self.location = None
         if uniqid is None:
-            self.item_type = item_type
             self.title = title
             self.uniqid = uniqid
             self.artist = artist
-            self.genre = genre
             self.year = year
-            self.rate = 0
-            self.location = None
-            self.conn.insert("Items", None, user_id, item_type, title, uniqid, artist, genre, year, None, 0)
-            self.id = self.db_conn.execute("select last_insert_rowid()").fetchone()[0]
-
+            db.insert("Items", ('owner', 'type', 'title', 'uniqueid', 'artist', 'genre', 'year'), user_id, item_type, title, uniqid, artist, genre, year)
+            self.id = db_cur.execute("select last_insert_rowid()").fetchone()[0]
         else:
             metadata = None
             try:
@@ -48,48 +54,84 @@ class Item:
                 self.title = metadata['title']
                 self.year = metadata['Year']
                 self.artist = metadata['Authors'][0]
-                self.item_type = item_type
-                self.genre = genre
-                self.rate = 0
-                self.location = None
-                self.db_conn.insert("Items", None, user_id, item_type, self.title, uniqid, self.artist, self.genre,
-                                    self.year, None, 0)
+                db.insert("Items", ('owner', 'type', 'title', 'uniqueid', 'artist', 'genre', 'year'), user_id, item_type, self.title, uniqid, self.artist, self.genre,
+                                    self.year)
+                self.id = db_cur.execute("select last_insert_rowid()").fetchone()[0]
 
     def borrowed_req(self, user):
-        self.conn.insert("Requests", None, user.id, self.id, datetime.now(), self.REQUEST_TYPES[1][0])
+        db_cur = db.get_cursor()
+        db.insert("BorrowRequests", ('user_id', 'item_id', 'request_date'), user.id, self.id, datetime.now())
+        fetched_users = db_cur.execute('select user_id from BorrowRequests order by datetime(request_time) where item_id={item} ;'.format(item=self.id)).fetchall()
+        print(fetched_users)
+        return fetched_users
 
     def borrowed_by(self, user, return_date=2):
+        db_cur = db.get_cursor()
         taking_date = datetime.now()
         return_date = taking_date + timedelta(weeks=return_date)
-        self.conn.insert("Borrows", None, user.id, self.id, datetime.now(), return_date, 0)
+        db.insert("Borrows", ('user_id', 'item_id', 'taking_date', 'return_date', 'is_returned'), user.id, self.id, datetime.now(), return_date, 0)
+        db_cur.execute('delete from BorrowRequests where user_id={user} and item_id={item} ;'.format(user=user.id, item=self.id ))
+        db.connection.commit()
 
     def returned(self, location=None):
-        self.db_conn.execute("update Borrows set is_returned=1 where is_returned=0 and item_id={id};".format(id=self.id))
+        db_cur = db.get_cursor()
+        db_cur.execute("update Borrows set is_returned=1 where is_returned=0 and item_id={id};".format(id=self.id))
         self.location = location
-        self.db_conn.execute("update Items set location=\'{loc}\' where id={id};".format(loc=location, id=self.id))
+        db.execute("update Items set location=\'{loc}\' where id={id};".format(loc=location, id=self.id))
+        # TODO:
+        # send notification to users who watches the item to borrow
+        # delete watchrequest after notification
         self.conn.connection.commit()
 
     def comment(self, user, comment_text):
-        self.conn.insert("Comments", None, user.id, self.id, comment_text, datetime.now())
+        db.insert("Comments", ('user_id', 'item_id', 'comment', 'date'), user.id, self.id, comment_text, datetime.now())
+        # TODO:
+        # send notification to users who watches the item to watch
 
     def list_comments(self):
-
+        db_cur = db.get_cursor()
+        fetched_comments = db_cur.execute('select user_id, comment from Comments order by datetime(date) where item_id=\'{item}\''.format(item=self.id)).fetchall()
+        print(fetched_comments)
+        return fetched_comments
 
     def rate(self, user, rating):
-        self.rate += 1
-        pass
+        db_cur = db.get_cursor()
+        db_cur.execute('update Borrows set rate={rate} where item_id={item} and user_id ={user} and is_returned=1 ; '.format(user=user.id, item=self.id))
+        db.connection.commit()
 
     def get_rating(self):
-
-        pass
+        db_cur = db.get_cursor()
+        avg_rating = db_cur.execute('select avg(rate) from Borrows where item_id={item} and rate is not null;').fetchone()[0]
+        print(avg_rating)
+        return avg_rating
 
     def locate(self, location):
-        pass
+        db_cur = db.get_cursor()
+        db_cur.execute("update Items set location=\'{loc}\' where id={id};".format(loc=location, id=self.id))
+        db.connection.commit()
 
     def setstate(self, state_type, state):
-        pass
+        db_cur = db.get_cursor()
+        db_cur.execute("update Items set {state_type}={state} where item_id={item}".format(item=self.id, state_type=state_type, state=self.STATE_TYPE[state]))
+        db.connection.commit()
 
-    def search(self, user, search_text, genre, year, for_borrow=False):
+    @classmethod
+    def search(cls, user, search_text, genre, year, for_borrow=False):
+        db_cur = db.get_cursor()
+        words_text = list(search_text.split(" "))
+        # in sqlite default like statement is case insensitive already
+        state_friend = db_cur.execute('select state from Friends where sender_user={user} or receiver_user={user}').fetchone()[0]
+        if year:
+            dt = datetime(year=year, month=1, day=1, hour=0, minute=0, second=0, microsecond=0)
+            print(dt)
+            if for_borrow:
+                state_friend = self.db_conn.execute('select state from Friends where (sender_user={user} and receiver_user=) or receiver_user={user}').fetchone()[0]
+                f_string = f'select user_id, item_id from Items where {"artist like ? " * len(words_text)} or {"title like ? " * len(words_text)} where datetime(year)>={"?"} and borrow>=;'
+            else:
+                f_string = f'select user_id, item_id from Items where {"artist like ? "*len(words_text)} or {"title like ? "*len(words_text)} where datetime(year)>={"?"} ;'
+        else:
+            f_string = f'select user_id, item_id from Items where {"artist like ? " * len(words_text)} or {"title like ? " * len(words_text)} where '
+        db_cur.execute('select user_id, item_id from Items where artist in  and ')
         pass
 
     def watch(self, user, watch_method):
